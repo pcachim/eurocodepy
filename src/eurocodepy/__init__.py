ed63b0b1c5e2faa639dc52e4b8f8f8fb794a6522
Paulo Cachim <pcachim@ua.pt>
2022-02-10 23:11:36 +0000

    20220210

--- src/eurocodepy/__init__.py
+++ src/eurocodepy/__init__.py
@@ -8,7 +8,8 @@ from .ec_base import ReinforcementBars
 from .ec_base import ReinforcementClasses
 from .ec_base import ConcreteClasses
 
-#from .ec_base import get_database
+from .ec_base import get_database
+
 from . import ec2
 from . import ec5
 from . import utils
