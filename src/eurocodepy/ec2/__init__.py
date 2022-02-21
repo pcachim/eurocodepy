08335f9da2b0e215d02c09a7c6e2d79384129c1b
Paulo Cachim <pcachim@ua.pt>
2022-02-21 16:31:24 +0000

    202202211631

--- src/eurocodepy/ec2/__init__.py
+++ src/eurocodepy/ec2/__init__.py
@@ -11,6 +11,6 @@ from .sls.longterm import shrink_strain
 from .uls.shear import shear_asws
 from .uls.shear import shear_vrd
 from .uls.shear import shear_vrdc
-
+from .uls.bend_simple import bend_params
 # from .ec2_crack import *
 # from .ec2_deformation import *
