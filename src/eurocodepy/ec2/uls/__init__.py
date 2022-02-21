2829b6690da98d8ebfa937ce6b9c613aad3a4f47
Paulo Cachim <pcachim@ua.pt>
2022-02-21 16:54:45 +0000

    fixed gitignore 20220221

--- src/eurocodepy/ec2/uls/__init__.py
+++ src/eurocodepy/ec2/uls/__init__.py
@@ -1,8 +1,8 @@
-from . import shear
-from . import bend_simple
-
 from .shear import shear_asws
 from .shear import shear_vrd
 from .shear import shear_vrdc
 
 from .bend_simple import bend_params
+
+from . import shear
+from . import bend_simple
