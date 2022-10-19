75b603da899c0c5766af9da970c4221e53adb640
Paulo Cachim <pcachim@ua.pt>
2022-10-19 14:57:48 +0100

    20221019 v.0.1.00 again

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, Paulo Cachim'
 author = 'Paulo Cachim'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.15'
+release = '0.1.00'
 
 
 # -- General configuration ---------------------------------------------------
